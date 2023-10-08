e668bf60426ba82181fc08b7d3ff5470f0eeb1d1
Jaime <quezadajaime@ymail.com>
2023-10-07 22:03:38 -0700

    blogRoutes

---
+++ controllers/api/blogPostRoutes.js
@@ -0,0 +1,64 @@
+const router = require("express").Router();
+const { BlogPost } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+//Route to create a new blog post
+router.post("/", withAuth, async (req, res) => {
+    console.log(req.body);
+    try {
+        const newBlogPost = await BlogPost.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newBlogPost);
+    } catch (err) {
+        console.log(err)
+        res.status(400).json(err);
+    }
+});
+
+//Route to edit an already existing blog post
+router.put("/:id", withAuth, async (req, res) => {
+    console.log(req.body);
+    try {
+        const blogPostData = await BlogPost.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!blogPostData) {
+            res.status(404).json({ message: "No blog post found with this ID" });
+            return;
+        }
+
+        res.status(200).json(blogPostData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//route to delete and already existing blog
+
+router.delete("/:id", withAuth, async (req, res) => {
+    console.log(req.params.id);
+    try {
+        const blogPostData = await BlogPost.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!blogPostData) {
+            res.status(404).json({ message: "No blog post found with this ID" });
+            return;
+        }
+
+        res.status(200).json(blogPostData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
